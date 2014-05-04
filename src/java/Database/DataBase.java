6cf058452beff1e8f257ac6902a6f0e348a3dcdd
CarlosSosa343 <zjuansosa@hotmail.com>
2014-05-04 01:51:08 -0500

    limpio

--- src/java/Database/DataBase.java
+++ src/java/Database/DataBase.java
@@ -26,8 +26,7 @@ public class DataBase {
     public DataBase()
     {
          try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
+
              Class.forName("com.mysql.jdbc.Driver").newInstance();         
             
         } catch (Exception ex) {
